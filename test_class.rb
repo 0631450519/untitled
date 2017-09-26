4c6e3d444e3d424407f844d49a171bd20bd6aeda
viktorproroka <victor.proroka@gmail.com>
2017-09-26 12:09:59 +0300

    refactoring

--- test_class.rb
+++ test_class.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'selenium-webdriver'
 
-    class TestRegistration < Test::Unit::TestCase
+    class SmokeTest < Test::Unit::TestCase
       def setup
         @driver = Selenium::WebDriver.for :firefox
         @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
@@ -9,6 +9,7 @@ require 'selenium-webdriver'
 
       def registration_user
         @driver.navigate.to 'http://demo.redmine.org'
+        @wait.until {@driver.find_element(:class, 'register').displayed?}
         @driver.find_element(:class, 'register').click
         @wait.until {@driver.find_element(:id, 'user_login').displayed?}
         @login = ('login' + rand(99999).to_s)
@@ -20,7 +21,7 @@ require 'selenium-webdriver'
         @driver.find_element(:id, 'user_firstname').send_keys @first_name
         @last_name = 'Surname'
         @driver.find_element(:id, 'user_lastname').send_keys @last_name
-        @driver.find_element(:id, 'user_mail').send_keys (@login + '@asdasdasd.ua')
+        @driver.find_element(:id, 'user_mail').send_keys (@login + '@asd.ua')
         @driver.find_element(:name, 'commit').click
       end
 
@@ -34,6 +35,7 @@ require 'selenium-webdriver'
 ##################################################################################
       def login_user
         @driver.navigate.to 'http://demo.redmine.org'
+        @wait.until {@driver.find_element(:class, 'login').displayed?}
         @driver.find_element(:class, 'login').click
         @wait.until {@driver.find_element(:id, 'username').displayed?}
         @driver.find_element(:id, 'username').send_keys @login
@@ -43,6 +45,7 @@ require 'selenium-webdriver'
 
       def test_login_user
         registration_user
+        logout_user
         login_user
         @wait.until {@driver.find_element(:id, 'loggedas').displayed?}
         expected_text = "Logged in as #{@login}"
@@ -57,7 +60,6 @@ require 'selenium-webdriver'
 
       def test_logout_user
         registration_user
-        login_user
         logout_user
         @wait.until {@driver.find_element(:class, 'login').displayed?}
         expected_button = 'Sign in'
@@ -81,7 +83,6 @@ require 'selenium-webdriver'
 
       def test_change_password
         registration_user
-        login_user
         change_password
         @wait.until {@driver.find_element(:id, 'flash_notice').displayed?}
         expected_text = 'Password was successfully updated.'
@@ -90,13 +91,10 @@ require 'selenium-webdriver'
       end
 #########################################################################################3
       def create_project
-        #@wait.until {@driver.find_element(:xpath, "//*[text()='Projects']").displayed?}
-        sleep(1)
+        @wait.until {@driver.find_element(:class, 'projects').displayed?}
         @driver.find_element(:class, 'projects').click
-        sleep(1)
         @wait.until {!@driver.find_elements(:xpath, "//*[text()='New project']").empty?}
         @driver.find_element(:xpath, "//*[text()='New project']").click
-        sleep(1)
         @wait.until {@driver.find_element(:id, 'project_name').displayed?}
         @project_name = 'Project' + rand(9999).to_s
         @driver.find_element(:id, 'project_name').send_keys @project_name
@@ -105,7 +103,6 @@ require 'selenium-webdriver'
 
       def test_create_project
         registration_user
-        login_user
         create_project
         @wait.until {@driver.find_element(:id, 'flash_notice').displayed?}
         expected_text = 'Successful creation.'
@@ -119,7 +116,6 @@ require 'selenium-webdriver'
         @wait.until {@driver.find_element(:class, 'settings').displayed?}
         @driver.find_element(:class, 'settings').click
         @wait.until {@driver.find_element(:id, 'tab-members').displayed?}
-        sleep(1)
         @driver.find_element(:id, 'tab-members').click
         @driver.find_element(:xpath, "//*[text()='New member']").click
         @wait.until {@driver.find_element(:id, 'principal_search').displayed?}
@@ -130,28 +126,24 @@ require 'selenium-webdriver'
         @driver.find_element(:id, 'member-add-submit').click
       end
 
-      def test_a_add_user_to_project
+      def test_add_user_to_project
         registration_user
         @first_name_for_adding = @first_name
         @last_name_for_adding = @last_name
         logout_user
-        sleep(1)
         registration_user
-        login_user
-        sleep (1)
         create_project
         add_user_to_project
-        @wait.until {@driver.find_element(:xpath, "//*[text()=' #{@first_name_for_adding} #{@last_name_for_adding}']").displayed?}
+        @wait.until {@driver.find_element(:xpath, "//*[text()='#{@first_name_for_adding} #{@last_name_for_adding}']").displayed?}
         expected_user = "#{@first_name_for_adding} #{@last_name_for_adding}"
-        actual_user = @driver.find_element(:xpath, "//*[text()=' #{@first_name_for_adding} #{@last_name_for_adding}']").text
+        actual_user = @driver.find_element(:xpath, "//*[text()='#{@first_name_for_adding} #{@last_name_for_adding}']").text
         assert_equal(expected_user,actual_user)
       end
 ####################################################################################
       def create_project_version
+        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         @driver.find_element(:id, 'project_quick_jump_box').find_element(:xpath, "//*[text()='#{@project_name}']").click
-        sleep(2)
         @driver.find_element(:class, 'settings').click
-        sleep(2)
         @driver.find_element(:id, 'tab-versions').click
         @driver.find_element(:xpath, "//*[text()='New version']").click
         @wait.until {@driver.find_element(:id, 'version_name').displayed?}
@@ -163,9 +155,7 @@ require 'selenium-webdriver'
 
       def test_create_project_version
         registration_user
-        login_user
         create_project
-        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         create_project_version
         expected_text = 'Successful creation.'
         actual_text = @driver.find_element(:id, 'flash_notice').text
@@ -173,8 +163,8 @@ require 'selenium-webdriver'
       end
 ############################################################################################
       def bug_creation
+        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         @driver.find_element(:id, 'project_quick_jump_box').find_element(:xpath, "//*[text()='#{@project_name}']").click
-        sleep(2)
         @driver.find_element(:class, 'new-issue').click
         @wait.until {@driver.find_element(:id, 'issue_subject').displayed?}
         @driver.find_element(:id, 'issue_subject').send_keys 'some_summary'
@@ -184,10 +174,7 @@ require 'selenium-webdriver'
 
       def test_bug_creation
         registration_user
-        login_user
-        @wait.until {@driver.find_element(:class, 'projects').displayed?}
         create_project
-        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         bug_creation
         bug_number = @driver.find_element(:xpath, '//h2').text
         cut_bug_number = (bug_number[3, 9])
@@ -197,8 +184,8 @@ require 'selenium-webdriver'
       end
 #############################################################################################
       def feature_creation
+        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         @driver.find_element(:id, 'project_quick_jump_box').find_element(:xpath, "//*[text()='#{@project_name}']").click
-        sleep(1)
         @driver.find_element(:class, 'new-issue').click
         @wait.until {@driver.find_element(:id, 'issue_tracker_id').displayed?}
         @driver.find_element(:id, 'issue_tracker_id').find_element(:css,"option[value='2']").click
@@ -209,10 +196,7 @@ require 'selenium-webdriver'
 
       def test_feature_creation
         registration_user
-        login_user
-        @wait.until {@driver.find_element(:class, 'projects').displayed?}
         create_project
-        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         feature_creation
         bug_number = @driver.find_element(:xpath, '//h2').text
         cut_bug_number = (bug_number[7, 14])
@@ -222,8 +206,8 @@ require 'selenium-webdriver'
       end
 ##############################################################################################
       def support_creation
+        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         @driver.find_element(:id, 'project_quick_jump_box').find_element(:xpath, "//*[text()='#{@project_name}']").click
-        sleep(1)
         @driver.find_element(:class, 'new-issue').click
         @wait.until {@driver.find_element(:id, 'issue_tracker_id').displayed?}
         @driver.find_element(:id, 'issue_tracker_id').find_element(:css,"option[value='3']").click
@@ -234,10 +218,7 @@ require 'selenium-webdriver'
 
       def test_support_creation
         registration_user
-        login_user
-        @wait.until {@driver.find_element(:class, 'projects').displayed?}
         create_project
-        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         feature_creation
         bug_number = @driver.find_element(:xpath, '//h2').text
         cut_bug_number = (bug_number[7, 15])
@@ -247,12 +228,13 @@ require 'selenium-webdriver'
       end
 #############################################################################################
       def edit_users_role
+        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         @driver.find_element(:id, 'project_quick_jump_box').find_element(:xpath, "//*[text()='#{@project_name}']").click
         @wait.until {@driver.find_element(:class, 'settings').displayed?}
-        sleep(1)
+        #sleep(1)
         @driver.find_element(:class, 'settings').click
         @wait.until {@driver.find_element(:id, 'tab-members').displayed?}
-        sleep(1)
+        #sleep(1)
         @driver.find_element(:id, 'tab-members').click
         @wait.until {@driver.find_element(:xpath, "//*[text()='#{@first_name} #{@last_name}']").displayed?}
 
@@ -270,10 +252,7 @@ require 'selenium-webdriver'
 
       def test_edit_users_role
         registration_user
-        login_user
-        sleep (1)
         create_project
-        @wait.until {@driver.find_element(:id, 'project_quick_jump_box').displayed?}
         edit_users_role
         @wait.until {@driver.find_element(:xpath, '//td/span').displayed?}
         current_role = @driver.find_element(:xpath, '//td/span').text
